d806aa9339277a48137bceebde00c13ac9454aeb
Varsha Fernandes <varshafernandes90@gmail.com>
2026-02-25 23:20:53 +0000

    make fields nullable

--- src/PaymentGateway.Api/Features/ProcessPayment/ProcessPaymentRequest.cs
+++ src/PaymentGateway.Api/Features/ProcessPayment/ProcessPaymentRequest.cs
@@ -1,12 +1,12 @@
 namespace PaymentGateway.Api.Features.ProcessPayment;
 
 public sealed record ProcessPaymentRequest(
-    string CardNumber,
-    int ExpiryMonth,
-    int ExpiryYear,
-    string Currency,
-    long Amount,
-    string Cvv)
+    string? CardNumber,
+    int? ExpiryMonth,
+    int? ExpiryYear,
+    string? Currency,
+    long? Amount,
+    string? Cvv)
 {
     public ProcessPaymentCommand ToCommand() =>
         new(CardNumber, ExpiryMonth, ExpiryYear, Currency, Amount, Cvv);
